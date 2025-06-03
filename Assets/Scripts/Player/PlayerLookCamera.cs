b84bd7713ca076adadbc93417578b477a1edc463
r3suurect10n <sosnin.a.a@yandex.ru>
2025-06-03 23:36:41 +0300

    Add player movement and door animations on HouseMap

--- Assets/Scripts/Player/PlayerLookCamera.cs
+++ Assets/Scripts/Player/PlayerLookCamera.cs
@@ -1,18 +1,35 @@
-using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerLookCamera : MonoBehaviour
 {
-    [Header("Components properties")]
-    [SerializeField] private Transform _player;
-    [SerializeField] private CinemachineCamera _playerCamera;
+    [Header("Components properties")]    
+    [SerializeField] private Transform _playerCamera;
 
     [Header("Mouse properties")]
-    [SerializeField] private float _mouseSensivity;
+    [SerializeField] private float _mouseSensivityX;
+    [SerializeField] private float _mouseSensivityY;
+
+    [Header("Camera properties")]    
+    [SerializeField] private float _maxCameraPitch;
+        
+    private Vector2 _mouseInput;
+    private float _cameraPitch = 0f;
 
     private void Update()
     {
-        _player.rotation = Quaternion.Euler(0, _playerCamera.transform.rotation.y, 0);
+        float yaw = _mouseSensivityX * _mouseInput.x;
+        transform.Rotate(Vector3.up * yaw);
+
+        float pitch = _mouseSensivityY * _mouseInput.y;
+        _cameraPitch -= pitch;
+        _cameraPitch = Mathf.Clamp(_cameraPitch, -_maxCameraPitch, _maxCameraPitch);
+        _playerCamera.localEulerAngles = new Vector3(_cameraPitch, 0f, 0f);
+    }
+
+    public void OnLook(InputAction.CallbackContext callbackContext)
+    {
+        _mouseInput = callbackContext.ReadValue<Vector2>();
     }
 
 }
